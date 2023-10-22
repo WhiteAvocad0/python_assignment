4beaf2c9cd17fb59e7ff70ad889afd5eae292f4b
Jeremy <woonjeremy10@gmail.com>
2023-10-22 15:53:59 +0000

    Added function to modify suppliers

--- main.py
+++ main.py
@@ -424,7 +424,7 @@ def menu(user_type):
     if user_type == "admin":
         print(f"\tINVENTORY MANAGEMENT SYSTEM (Admin)\n\t{'+'*34}")
         while True:
-            select = input("\t1. Item Inventory Update\n\t2. Item Inventory Tracking\n\t3. Search distribution list\n\t4. Add new user\n\t5. Delete user\n\t6. Search user\n\t7. Modify User\n\t8. Update hospital\n\t9. Logout\n> ")
+            select = input("\t1. Item Inventory Update\n\t2. Item Inventory Tracking\n\t3. Search distribution list\n\t4. Add new user\n\t5. Delete user\n\t6. Search user\n\t7. Modify User\n\t8. Update hospital\n\t9. Update supplier\n\t10. Backup\n> ")
             try:
                 #Check if user entered anythings other than number
                 select = int(select)
@@ -450,6 +450,8 @@ def menu(user_type):
                 case 8:
                     update_hospital()
                 case 9:
+                    update_supplier()
+                case 10:
                     backup()
                 case _:
                     print("Invalid selection! Please try again.") 
@@ -597,24 +599,64 @@ def update_hospital():
 
         config_save("hospitals.txt","w",data_list)
 
+#Modify supplier
 def update_supplier():
     data_list = readfiles("suppliers.txt")
     while True:
-        selection = input("\tPlease select an option (Leave empty to quit):\n\t1. Add Supplier\n\t2. Change Supplier name\n\t3. Delete Supplier")
+        selection = input("\tPlease select an option (Leave empty to quit):\n\t1. Add Supplier\n\t2. Change Supplier name\n\t3. Delete Supplier\n\t> ")
         if not selection:
             return
         try:
-            selection.isdigit()
-            break
+            selection = int(selection)
         except ValueError:
             print("Please enter only number!")
             continue
-    match selection:
-        case 1:
-            for c,ele in enumerate(data_list[1],1):
-                print(f"{c}. {ele}")
-            
-            
+        match selection:
+            case 1:
+                if len(data_list[0]) == 4:
+                    print("Maximum suppliers is 4.")
+                else:
+                    data_list[0].append(f"S{len(data_list[1]) + 1}")
+                    data_list[1].append(input("Please enter supllier name: "))
+            case 2:
+                for c,ele in enumerate(data_list[1],1):
+                    print(f"{c}. {ele}")
+                while True:
+                    selection = input("Please select a supplier: ")
+                    if not selection:
+                        return
+                    try:
+                        selection = int(selection)
+                        break
+                    except ValueError:
+                        print("Please enter only number!")
+                        continue
+                new_name = input("Please enter new name (Leave empty to quit): ")
+                if not new_name:
+                    return
+                else:
+                    data_list[1][selection-1] = new_name
+            case 3:
+                if len(data_list[0]) == 3:
+                    print("Minimum suppliers is 3")
+                else:
+                    for c,ele in enumerate(data_list[1],1):
+                        print(f"{c}. {ele}")
+                    while True:
+                        delete = input("Please select a supplier to delete: ")
+                        if not delete:
+                            return
+                        try:
+                            delete = int(delete)
+                            break
+                        except ValueError:
+                            print("Please enter only number!")
+                            continue
+                    data_list[1].pop(delete-1)
+                    data_list[0].pop()
+            case _:
+                print("Please select a valid option")
+        config_save("suppliers.txt","w",data_list)
 
 #save config for ppe.txt    
 def config_save(fileName,mode,data_list):
